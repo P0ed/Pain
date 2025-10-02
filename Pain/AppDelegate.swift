33dc8772516e756aa07e059047f83878ea98c6e0
Konstantin Sukharev <poed@me.com>
2025-10-02 22:09:23 +0200

    Initial Commit

---
+++ Pain/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Pain
+//
+//  Created by Konstantin Sukharev on 02.10.2025.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
