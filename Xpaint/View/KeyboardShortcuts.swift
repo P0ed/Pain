80091c144bae2b853e0bcbace26f653b21f69a6d
Konstantin Sukharev <poed@me.com>
2025-12-04 23:37:58 +0100

    Match Preview scale behavior

--- Xpaint/View/KeyboardShortcuts.swift
+++ Xpaint/View/KeyboardShortcuts.swift
@@ -32,8 +32,8 @@ extension EditorView {
 			case "7", "&": numAction(6)
 			case "8", "*": numAction(7)
 
-			case "9": setScale(1.0)
-			case "0": setScale(file.size.zoomToFit(state.size))
+			case "9": setScale(file.size.zoomToFit(state.size))
+			case "0": setScale(1.0)
 			case "-": setScale(state.magnification / 2.0)
 			case "=": setScale(state.magnification * 2.0)
 
