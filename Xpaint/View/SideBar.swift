b2abe96bf7d036583c493978b8a05162ebdf70a8
Konstantin Sukharev <poed@me.com>
2025-12-12 21:25:38 +0100

    Add swap colors action

--- Xpaint/View/SideBar.swift
+++ Xpaint/View/SideBar.swift
@@ -5,12 +5,14 @@ extension EditorView {
 	var sidebar: some View {
 		ScrollView(.vertical) {
 			VStack(spacing: 0.0) {
-				ColorsView(colors: state.colors)
+				ColorsView(colors: state.colors) { _, _ in
+					state.swapColors()
+				}
 				Toggle("Dither", isOn: $state.dither)
 					.keyboardShortcut(KeyEquivalent("d"), modifiers: [])
 					.padding(8.0)
 				Spacer(minLength: 4.0)
-				ColorsView(colors: palette.colors) { color in
+				ColorsView(colors: palette.colors) { _, color in
 					state.primaryColor = color
 				}
 			}
@@ -22,16 +24,16 @@ extension EditorView {
 
 struct ColorsView: View {
 	var colors: [Px]
-	var didTap: (Px) -> Void = ø
+	var didTap: (Int, Px) -> Void = ø
 
 	var body: some View {
 		ForEach(
 			colors.enumerated(),
 			id: \.offset,
-			content: { _, color in
+			content: { idx, color in
 				color.ui
 					.frame(width: 128.0, height: 24.0)
-					.onTapGesture { didTap(color) }
+					.onTapGesture { didTap(idx, color) }
 			}
 		)
 	}
